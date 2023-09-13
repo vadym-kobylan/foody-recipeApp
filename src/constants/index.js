a3335b7401703bfe6054ff54c23a719ab1b220e4
Vadym Kobylan <vadym.kobylan@gmail.com>
2023-09-13 23:17:40 +0300

    Foody release

---
+++ src/constants/index.js
@@ -0,0 +1,204 @@
+export const categoryData = [
+  {
+    idCategory: '1',
+    strCategory: 'Beef',
+    strCategoryDescription:
+      'Beef is the culinary name for meat from cattle, particularly skeletal muscle. Humans have been eating beef since prehistoric times.[1] Beef is a source of high-quality protein and essential nutrients.[2]',
+    strCategoryThumb:
+      'https://www.simplyrecipes.com/thmb/ua7tAGC4K28qqcnTytOUaiTJdgI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Roast-Beef-SEO-1de45b2fe5244c399012bf7941dc40cc.jpg',
+  },
+  {
+    idCategory: '2',
+    strCategory: 'Chicken',
+    strCategoryDescription:
+      'Chicken is a type of domesticated fowl, a subspecies of the red junglefowl. It is one of the most common and widespread domestic animals, with a total population of more than 19 billion as of 2011.[1] Humans commonly keep chickens as a source of food (consuming both their meat and eggs) and, more rarely, as pets.',
+    strCategoryThumb:
+      'https://hips.hearstapps.com/hmg-prod/images/dutch-oven-roast-chicken-recipe-2-1664219307.jpg?crop=0.6666666666666667xw:1xh;center,top&resize=1200:*',
+  },
+  {
+    idCategory: '3',
+    strCategory: 'Dessert',
+    strCategoryDescription:
+      'Dessert is a course that concludes a meal. The course usually consists of sweet foods, such as confections dishes or fruit, and possibly a beverage such as dessert wine or liqueur, however in the United States it may include coffee, cheeses, nuts, or other savory items regarded as a separate course elsewhere. In some parts of the world, such as much of central and western Africa, and most parts of China, there is no tradition of a dessert course to conclude a meal. The term dessert can apply to many confections, such as biscuits, cakes, cookies, custards, gelatins, ice creams, pastries, pies, puddings, and sweet soups, and tarts. Fruit is also commonly found in dessert courses because of its naturally occurring sweetness. Some cultures sweeten foods that are more commonly savory to create desserts.',
+    strCategoryThumb:
+      'https://insanelygoodrecipes.com/wp-content/uploads/2021/01/White-Chocolate-Oreo-Cookie-Balls.png',
+  },
+  {
+    idCategory: '4',
+    strCategory: 'Lamb',
+    strCategoryDescription:
+      'Lamb, hogget, and mutton are the meat of domestic sheep (species Ovis aries) at different ages. A sheep in its first year is called a lamb, and its meat is also called lamb. The meat of a juvenile sheep older than one year is hogget; outside the USA this is also a term for the living animal. The meat of an adult sheep is mutton, a term only used for the meat, not the living animals. The term mutton is almost always used to refer to goat meat in the Indian subcontinent. ',
+    strCategoryThumb: 'https://wonderfulcook.com/wp-content/uploads/2022/12/lamb-chop-recipes.jpg',
+  },
+  {
+    idCategory: '5',
+    strCategory: 'Miscellaneous',
+    strCategoryDescription: "General foods that don't fit into another category",
+    strCategoryThumb:
+      'https://images-gmi-pmc.edge-generalmills.com/e59f255c-7498-4b84-9c9d-e578bf5d88fc.jpg',
+  },
+];
+export const mealData = [
+  {
+    idMeal: '52874',
+    strMeal: 'Beef and Mustard Pie',
+    idCategory: '1',
+    strMealThumb:
+      'https://www.greatbritishfoodawards.com/images/made/assets/images/recipes/sausage_and_mustard_pie_540_650_s_c1.jpg',
+  },
+  {
+    idMeal: '52878',
+    strMeal: 'Beef and Oyster pie',
+    idCategory: '1',
+    strMealThumb:
+      'https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/beef_shin_and_oyster_pie_42507_16x9.jpg',
+  },
+  {
+    idMeal: '53071',
+    strMeal: 'Beef Asado',
+    idCategory: '1',
+    strMealThumb:
+      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2009/7/10/0/0068299-01_Barbecued-Short-Ribs_s4x3.jpg.rend.hgtvcom.616.462.suffix/1382539150963.jpeg',
+  },
+  {
+    idMeal: '52997',
+    strMeal: 'Beef Banh Mi Bowls with Sriracha Mayo, Carrot & Pickled Cucumber',
+    idCategory: '1',
+    strMealThumb:
+      'https://marleyspoon.com/media/recipes/119643/main_photos/large/beef_banh_mi_rice_bowl-dcdad6e0081eec9f0dcb07ecae5efe67.jpeg',
+  },
+  {
+    idMeal: '52904',
+    strMeal: 'Beef Bourguignon',
+    idCategory: '1',
+    strMealThumb:
+      'https://www.spendwithpennies.com/wp-content/uploads/2021/12/Beef-Bourguignon-SpendWithPennies-11.jpg',
+  },
+
+  {
+    idMeal: '53050',
+    strMeal: 'Ayam Percik',
+    idCategory: '2',
+    strMealThumb:
+      'https://tasteasianfood.com/wp-content/uploads/2022/07/ayam-percik-recipe-square.jpeg',
+  },
+  {
+    idMeal: '52940',
+    strMeal: 'Brown Stew Chicken',
+    idCategory: '2',
+    strMealThumb:
+      'https://www.allrecipes.com/thmb/Nbw5sze82YEZMf_lMm62l83HPhs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/8589870-jamaican-brown-stew-chicken-Chef-John-1x1-1-d75eb0ca746c429d91746e5debbb461c.jpg',
+  },
+  {
+    idMeal: '53016',
+    strMeal: 'Chick-Fil-A Sandwich',
+    idCategory: '2',
+    strMealThumb:
+      'https://media.cnn.com/api/v1/images/stellar/prod/230818112135-chick-fil-a-honey-pepper-pimento-chicken-sandwich.jpg?c=16x9&q=h_720,w_1280,c_fill',
+  },
+  {
+    idMeal: '52846',
+    strMeal: 'Chicken & mushroom Hotpot',
+    idCategory: '2',
+    strMealThumb:
+      'https://realfood.tesco.com/media/images/RFO-1400x919-Chicken-mushroom-hotpot-9c3b4f78-fabb-43c3-8da6-a190d8e75f68-0-1400x919.jpg',
+  },
+  {
+    idMeal: '52796',
+    strMeal: 'Chicken Alfredo Primavera',
+    idCategory: '2',
+    strMealThumb:
+      'https://images-gmi-pmc.edge-generalmills.com/9f86272f-e630-4582-9140-c5127679c35e.jpg',
+  },
+
+  {
+    idMeal: '52893',
+    strMeal: 'Apple & Blackberry Crumble',
+    idCategory: '3',
+    strMealThumb: 'https://www.yummyfruit.co.nz/wp-content/uploads/2018/08/apple-berry-crumble.jpg',
+  },
+  {
+    idMeal: '52768',
+    strMeal: 'Apple Frangipan Tart',
+    idCategory: '3',
+    strMealThumb:
+      'https://www.abakingjourney.com/wp-content/uploads/2022/10/Apple-Frangipane-Tart-Feature.jpg',
+  },
+  {
+    idMeal: '52767',
+    strMeal: 'Bakewell tart',
+    idCategory: '3',
+    strMealThumb:
+      'https://www.deliciousmagazine.co.uk/wp-content/uploads/2018/09/861186-1-eng-GB_bakewell-tart-768x951.jpg',
+  },
+  {
+    idMeal: '52855',
+    strMeal: 'Banana Pancakes',
+    idCategory: '3',
+    strMealThumb:
+      'https://www.allrecipes.com/thmb/6x0Lw9L4MEU8INHnK4tXGRV9XWI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/20334-banana-pancakes-i-DDMFS-4x3-9f291f03044247d48c9ec26917952402.jpg',
+  },
+  {
+    idMeal: '52894',
+    strMeal: 'Battenberg Cake',
+    idCategory: '3',
+    strMealThumb:
+      'https://food-images.files.bbci.co.uk/food/recipes/battenburg_cake_60878_16x9.jpg',
+  },
+
+  {
+    idMeal: '52769',
+    strMeal: 'Kapsalon',
+    idCategory: '4',
+    strMealThumb:
+      'https://s3.przepisy.pl/przepisy3ii/img/variants/800x0/img20230103174334jpg1672768088734.jpg',
+  },
+  {
+    idMeal: '52974',
+    strMeal: 'Keleya Zaara',
+    idCategory: '4',
+    strMealThumb:
+      'https://www.allrecipes.com/thmb/-r4rrv-UYcTa1bETOwt48rJLEHI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/4541392-9bef82aa607542c4b4843e092a939b3e.jpg',
+  },
+  {
+    idMeal: '53009',
+    strMeal: 'Lamb and Lemon Souvlaki',
+    idCategory: '4',
+    strMealThumb:
+      'https://recipes.net/wp-content/uploads/2023/07/lamb-lemon-dill-souvlaki_41cb4259bd6bd859dc4991b407e27ca4.jpeg',
+  },
+  {
+    idMeal: '52877',
+    strMeal: 'Lamb and Potato pie',
+    idCategory: '4',
+    strMealThumb:
+      'https://www.chefnotrequired.com/wp-content/uploads/2020/03/lamb-pie-blog-hero.jpg',
+  },
+
+  {
+    idMeal: '52943',
+    strMeal: 'Oxtail with broad beans',
+    idCategory: '5',
+    strMealThumb: 'https://cookingmaniac.com/wp-content/uploads/2016/06/plated-oxtail.jpg',
+  },
+  {
+    idMeal: '53017',
+    strMeal: 'Paszteciki (Polish Pasties)',
+    idCategory: '5',
+    strMealThumb:
+      'https://www.kwestiasmaku.com/sites/v123.kwestiasmaku.com/files/paszteciki_01.jpg',
+  },
+  {
+    idMeal: '52930',
+    strMeal: 'Pate Chinois',
+    idCategory: '5',
+    strMealThumb:
+      'https://cdn.pratico-pratiques.com/app/uploads/sites/4/2018/08/30192047/pate-chinois-au-fromage-en-grains.jpeg',
+  },
+  {
+    idMeal: '53042',
+    strMeal: 'Portuguese prego with green piri-piri',
+    idCategory: '5',
+    strMealThumb: 'https://www.sandwichtribunal.com/wp-content/uploads/2018/07/IMG_3094.jpg',
+  },
+];
